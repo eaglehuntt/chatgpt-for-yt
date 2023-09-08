8fb4c7b480dc4cff70e02d20591a6fdabdef86dc
eaglehuntt <eagle@eaglehunt.net>
2023-09-08 00:00:17 -0700

    Added function to parse the JSON transcript. It is working good. Known bug: once the background listener is removed, it stays removed.

--- background.js
+++ background.js
@@ -23,25 +23,49 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   }
 });
 
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    // Check if you want to process this request based on details.url or other criteria.
-    if (details.url.includes("youtube.com/api/timedtext")) {
-      // You can access details.url and other information of the request here.
+const onTranscriptApiCall = async (details) => {
+  if (details.url.includes("youtube.com/api/timedtext")) {
+    try {
+      const response = await fetch(details.url);
+      let data = await response.text();
+      data = JSON.parse(data);
 
-      // To manipulate the request or cancel it, you can use details.requestBody or details.responseHeaders.
-      // For example, you can modify headers before sending the request.
-      // To cancel the request, use `return { cancel: true };`
+      getTranscript(data);
 
-      // Here, you can perform actions on the intercepted request.
-      console.log("Intercepted request:", details.url);
-
-      // If you need to modify the request or response, you can do it here.
+      console.log(`Transcript: ${getTranscript(data)}`);
+    } catch (error) {
+      console.error("There was an error intercepting the transcript");
+      console.error(error);
     }
-  },
-  {
-    urls: ["https://www.youtube.com/api/timedtext*"], // Specify the URL patterns to intercept.
-    types: ["xmlhttprequest"], // Specify the types of requests to intercept.
-  },
-  []
-);
+    chrome.webRequest.onCompleted.removeListener(onTranscriptApiCall);
+  }
+};
+
+// Add the listener
+chrome.webRequest.onCompleted.addListener(onTranscriptApiCall, {
+  urls: ["https://www.youtube.com/api/timedtext*"], // Specify the URL patterns to intercept.
+  types: ["xmlhttprequest"], // Specify the types of requests to intercept.
+});
+
+const getTranscript = (data) => {
+  let transcript = "";
+
+  let events = data.events;
+
+  // iterate over every item in the events array
+  for (let i = 0; i < events.length; i++) {
+    // if that item has a segement
+    if (Object.hasOwn(events[i], "segs")) {
+      let segs = events[i].segs;
+      // iterate over all items in the segment array and add them to the transcript string
+      for (let j = 0; j < segs.length; j++) {
+        if (segs[j].utf8) {
+          transcript += `${segs[j].utf8} `;
+        }
+      }
+    }
+  }
+
+  transcript = transcript.trim();
+  return transcript;
+};
