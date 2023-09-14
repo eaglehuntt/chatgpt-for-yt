054434c246ef5e5470055860649e2dcbd00ea82a
eaglehuntt <eagle@eaglehunt.net>
2023-09-13 23:17:48 -0700

    Converted content-script to OOP

--- content-script.js
+++ content-script.js
@@ -1,34 +1,59 @@
 (() => {
-  const addGptButton = () => {
-    const gptButtonExists = document.getElementsByClassName("gpt-button")[0];
-
-    if (!gptButtonExists) {
-      const gptButton = document.createElement("img");
-
-      gptButton.style.cursor = "pointer";
-      gptButton.src = chrome.runtime.getURL("assets/bookmark.png"); // update later
-      gptButton.className = "ytp-button" + "gpt-button";
-      gptButton.title = "Click to start ChatGPT prompt";
-
-      const youtubeLeftControls =
-        document.getElementsByClassName("ytp-left-controls")[0];
-      youtubePlayer = document.getElementsByClassName("video-stream")[0];
-
-      youtubeLeftControls.append(gptButton);
-
-      gptButton.addEventListener("click", () => {
-        chrome.runtime.sendMessage({
-          type: "GPT_BUTTON_CLICKED",
-        });
-      });
+  class ContentScript {
+    constructor() {
+      if (window.location.href.includes("youtube.com/watch")) {
+        this.addGptButton();
+      }
     }
-  };
 
-  if (window.location.href.includes("youtube.com/watch")) {
-    addGptButton();
+    addGptButton() {
+      const gptButtonExists = document.getElementsByClassName("gpt-button")[0];
+
+      if (!gptButtonExists) {
+        const gptButton = document.createElement("img");
+
+        gptButton.style.cursor = "pointer";
+        gptButton.src = chrome.runtime.getURL("assets/bookmark.png"); // update later
+        gptButton.className = "ytp-button" + "gpt-button";
+        gptButton.title = "Click to start ChatGPT prompt";
+
+        const youtubeLeftControls =
+          document.getElementsByClassName("ytp-left-controls")[0];
+
+        //const youtubePlayer =
+        //  document.getElementsByClassName("video-stream")[0];
+
+        youtubeLeftControls.append(gptButton);
+
+        gptButton.addEventListener("click", () => {
+          chrome.runtime.sendMessage({
+            type: "GPT_BUTTON_CLICKED",
+          });
+        });
+      }
+    }
+
+    ensureClosedCaptionsActivated() {
+      // Select the CC button on the YouTube video player using aria-label.
+      // We need to do this in order to intercept the api call.
+      const ccButton = document.querySelector(
+        ".ytp-subtitles-button.ytp-button"
+      );
+
+      // Check if the CC button exists and whether it is pressed (activated).
+      if (ccButton && ccButton.getAttribute("aria-pressed") !== "true") {
+        // CC button is not activated, so activate and deactivate it quickly.
+        ccButton.click();
+
+        // Optional: Wait for a moment before deactivating (adjust timing as needed).
+        setTimeout(() => {
+          ccButton.click();
+        }, 500);
+      }
+    }
   }
 
-  let currentVideo = "";
+  const contentScript = new ContentScript();
 
   // Listen for a "NEW" message. This will indicate there is a new video
   chrome.runtime.onMessage.addListener((message, sender, response) => {
@@ -36,25 +61,8 @@
 
     if (type == "NEW_CHAT") {
       currentVideo = videoId;
-      ensureClosedCaptionsActivated();
+      contentScript.ensureClosedCaptionsActivated();
       // this is where the chatbox will be generated.
     }
   });
-
-  const ensureClosedCaptionsActivated = () => {
-    // Select the CC button on the YouTube video player using aria-label.
-    // We need to do this in order to intercept the api call.
-    const ccButton = document.querySelector(".ytp-subtitles-button.ytp-button");
-
-    // Check if the CC button exists and whether it is pressed (activated).
-    if (ccButton && ccButton.getAttribute("aria-pressed") !== "true") {
-      // CC button is not activated, so activate and deactivate it quickly.
-      ccButton.click();
-
-      // Optional: Wait for a moment before deactivating (adjust timing as needed).
-      setTimeout(() => {
-        ccButton.click();
-      }, 500);
-    }
-  };
 })();
